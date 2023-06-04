519ad976a8b5225fca33747ac33a54e652e3485f
Joanna K <joannakleinrm@gmail.com>
2023-06-04 21:01:10 +0200

    dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import css from './App.module.css'
+import { SearchBar } from './SearchBar/SearchBar';
+
+
+const key = `34935392-24250165e01040adac8554f89`
+const api = `https://pixabay.com/api/?key=${key}&image_type=photo&orientation=horizontal&per_page=12`
+
 
 export const App = () => {
   return (
     <div className={css.container}>
-      React homework template
+      <SearchBar/>
     </div>
   );
 };
