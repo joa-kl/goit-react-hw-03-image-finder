caa371a4d26548025d9d90f2421ffe2f919bf4cd
Joanna K <joannakleinrm@gmail.com>
2023-06-04 21:45:33 +0200

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,25 +2,21 @@ import { Component } from "react";
 
 
 
-export class SearchBar extends Component {
-    render() {
-        return (
-            < header class="searchbar" >
-                <form class="form">
-                    <button type="submit" class="button">
-                        <span class="button-label">Search</span>
-                    </button>
+export const SearchBar = (onSubmit) => (
+    < header class="searchbar" >
+        <form class="form" onSubmit={onSubmit}>
+            <button type="submit" class="button">
+                <span class="button-label">Search</span>
+            </button>
 
-                    <input
-                        class="input"
-                        type="text"
-                        autocomplete="off"
-                        autofocus
-                        placeholder="Search images and photos"
-                    />
-                </form>
-        </header >
-        )
-    }
-}   
-    
+            <input
+                class="input"
+                type="text"
+                autocomplete="off"
+                autofocus
+                placeholder="Search images and photos"
+            />
+        </form>
+    </header >
+)
+ 
