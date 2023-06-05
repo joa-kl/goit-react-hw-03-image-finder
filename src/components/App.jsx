05abbe1c65fa1b9357487705cce83487360a45c4
Joanna K <joannakleinrm@gmail.com>
2023-06-05 19:54:46 +0200

    api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,3 @@
-// import css from './App.module.css'
-
-
-
-// const key = `34935392-24250165e01040adac8554f89`
-// const api = `https://pixabay.com/api/?key=${key}&image_type=photo&orientation=horizontal&per_page=12`
-
-
-// export const App = () => {
-//   return (
-//     <div className={css.container}>
-//       <SearchBar/>
-//     </div>
-//   );
-// };
-
 import React, { Component } from "react";
 import axios from "axios";
 import { SearchBar } from './SearchBar/SearchBar';
@@ -21,45 +5,61 @@ import css from './Styles.module.css'
 import { fetchImages } from "./api/api";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 
-const key = "34935392-24250165e01040adac8554f89";
-axios.defaults.baseURL = `https://pixabay.com/api/?key=${key}`;
+// const key = "34935392-24250165e01040adac8554f89";
+// axios.defaults.baseURL = `https://pixabay.com/api/?key=${key}`;
 
 
 export class App extends Component {
   state = {
     images: [],
     isLoading: false,
-    currentSearch: "",
+    query: "",
     pageNr: 1,
+    error: null,
   };
 
-  async componentDidMount() {
-    // const response = await axios.get("&image_type=photo&orientation=horizontal&per_page=12");
-    // const response = await axios.get(`https://pixabay.com/api/?key=${key}&image_type=photo&orientation=horizontal&per_page=12`);
-    const response = await axios.get(`https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&image_type=photo&pretty=true&per_page=12`);
-    this.setState({ images: response.data.hits });
-  }
+  // async componentDidMount() {
+  //   // const response = await axios.get("&image_type=photo&orientation=horizontal&per_page=12");
+  //   // const response = await axios.get(`https://pixabay.com/api/?key=${key}&image_type=photo&orientation=horizontal&per_page=12`);
+  //   const response = await axios.get(`https://pixabay.com/api/?key=34935392-24250165e01040adac8554f89&image_type=photo&pretty=true&per_page=12`);
+  //   this.setState({ images: response.data.hits });
+  // }
 
    handleInputChange = evt => {
     const { name, value } = evt.target;
     this.setState({ [name]: value });
   };
 
-  handleSearchQuerySubmit = evt => {
+  handleSearchQuerySubmit = async evt => {
     evt.preventDefault();
-    this.setState({ isLoading: true });
-    const inputForSearch = evt.target.elements.inputForSearch;
-    if (inputForSearch.value.trim() === '') {
-      return;
-    }
-    const response = fetchImages(inputForSearch.value, 1);
-    this.setState({
-      images: response,
-      isLoading: false,
-      currentSearch: inputForSearch.value,
-      pageNr: 1,
-    });
+    this.getImages(this.state.query);
+    // this.setState({ isLoading: true });
+    // const inputForSearch = evt.target.elements.inputForSearch;
+    // console.log(inputForSearch);
+    // if (inputForSearch.value.trim() === '') {
+    //   return;
+    // }
+    // const response = await fetchImages("dog", 1);
+    // this.setState({
+    //   images: response,
+    //   isLoading: false,
+    //   input: input.value,
+    //   // currentSearch: inputForSearch.value,
+    //   pageNr: 1,
+    // });
   };
+
+  getImages = async (query) => {
+    this.setState({ loading: true });
+    try {
+      const images = await fetchImages(query);
+      this.setState({ images });
+    } catch (err) {
+      this.setState({ error: err });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   handleImageClick = (evt) => {
     this.setState({
@@ -68,22 +68,7 @@ export class App extends Component {
       modalImg: evt.target.name,
     })
   }
-    // const id = nanoid();
-    // const name = evt.name;
-    // const number = evt.number;
-  //   const images = [this.state.images];
-  //   // const doesExist = contactsLists.findIndex(contact => name === contact.name) !== -1;
 
-  //   // doesExist
-  //   //   ? alert(`${name} is already in contacts.`)
-  //   //   : contactsLists.push({  name, number });
-    
-  //   this.setState({ images: images });
-  // };
-
-  // submitSearchQuery() {
-
-  // }
 
   render() {
     const { images } = this.state;
--- src/components/api/api.js
+++ src/components/api/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 axios.defaults.baseURL = `https://pixabay.com/api`;
 const key = "34935392-24250165e01040adac8554f89";
 
-export const fetchImages = async (inputValue, pageNr) => {
+export const fetchImages = async (query, pageNr) => {
     const response = await axios.get(
-        `/?q=${inputValue}&page=${pageNr}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
+        `/?q=${query}&page=${pageNr}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`
     );
     return response.data.hits.map(image => {
         return {
